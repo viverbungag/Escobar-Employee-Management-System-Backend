dd69c9c9c0d56a2ccc73e62d5bb16f3ecb18152f
viver <viverbungag1@gmail.com>
2022-09-02 18:08:05 +0800

    Initialized spring boot project

---
+++ src/test/java/com/exe/EMS/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.EMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
