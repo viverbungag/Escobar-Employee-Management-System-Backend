a790017d5ec02a0b8606a0832d29495b1dc0593f
viver <viverbungag1@gmail.com>
2022-09-17 00:17:17 +0800

    Added exception handlers

---
+++ src/main/java/com/exe/EMS/exceptionHandlers/ApiException.java
@@ -0,0 +1,14 @@
+package com.exe.EMS.exceptionHandlers;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public class ApiException {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+}
