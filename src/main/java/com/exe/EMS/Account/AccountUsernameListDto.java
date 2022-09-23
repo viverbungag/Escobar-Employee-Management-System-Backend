f63228a5bafa88f8d9fe33a2ff1a69d7402c0291
viver <viverbungag1@gmail.com>
2022-09-23 16:49:59 +0800

    Fixed the activate and inactivate of account

--- src/main/java/com/exe/EMS/Account/AccountUsernameListDto.java
+++ src/main/java/com/exe/EMS/Account/AccountUsernameListDto.java
@@ -2,6 +2,7 @@ package com.exe.EMS.Account;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class AccountUsernameListDto {
 
     List<String> accountUsernames;
--- src/main/java/com/exe/EMS/Security/SecurityMySqlRepository.java
+++ src/main/java/com/exe/EMS/Security/SecurityMySqlRepository.java
@@ -11,10 +11,11 @@ import java.util.Optional;
 @Repository("security_mysql")
 public interface SecurityMySqlRepository extends SecurityDao, JpaRepository<Account, Long> {
 
-    @Query(value = "SELECT * FROM #{#entityName} " +
+    @Query(value = "SELECT * FROM #{#entityName} AS account " +
             "INNER JOIN employee AS employee ON account.employee_id = employee.employee_id " +
             "INNER JOIN employee_type as employee_type ON employee.employee_type_id = employee_type.employee_type_id " +
-            "WHERE account_username = :accountUsername AND account_password = :accountPassword AND employee_type_name != 'Owner'",
+            "WHERE account_username = :accountUsername AND account_password = :accountPassword " +
+            "AND employee_type_name != 'Owner' AND account.is_active = true",
             nativeQuery = true)
     Optional<Account> findEmployeeUserByNameAndPassword(@Param("accountUsername") String accountUsername,
                                                 @Param("accountPassword") String accountPassword);
@@ -22,7 +23,8 @@ public interface SecurityMySqlRepository extends SecurityDao, JpaRepository<Acco
     @Query(value = "SELECT * FROM #{#entityName} AS account " +
             "INNER JOIN employee AS employee ON account.employee_id = employee.employee_id " +
             "INNER JOIN employee_type as employee_type ON employee.employee_type_id = employee_type.employee_type_id " +
-            "WHERE account_username = :accountUsername AND account_password = :accountPassword AND employee_type_name = 'Owner'",
+            "WHERE account_username = :accountUsername AND account_password = :accountPassword " +
+            "AND employee_type_name = 'Owner' AND account.is_active = true",
             nativeQuery = true)
     Optional<Account> findAdminUserByNameAndPassword(@Param("accountUsername") String accountUsername,
                                                 @Param("accountPassword") String accountPassword);
